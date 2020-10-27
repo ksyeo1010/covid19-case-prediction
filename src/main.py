8dfe3f510ff097eda08c8b846e1bf4dd0fc8512c
Kwangsoo <ksyeo1010@gmail.com>
2020-10-26 20:45:00 -0700

    working linear regressor:

--- src/main.py
+++ src/main.py
@@ -4,106 +4,67 @@ import argparse
 import time
 import pickle
 import pandas as pd
+import datetime as dt
 
 # cs-340 helper code
 from utils import utils
 
 # 3rd party libraries
 import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
 
 # my modules
 from model.linear_model import LeastSquares, LeastSquaresPoly
 from model.auto_regressor import AutoRegressor
-from model.knn import KNN
 
-
-def __polyBasis(X, p):
-    N = X.size
-    Z = np.ones((N, p))
-    for j in range(1, p + 1):
-        # Z = [x_i**0 x_i**1 ... x_i**p]
-        Z[:,j-1] = X**j
-    return Z
-
-def __sinBasis(X):
-    N = X.size
-    Z = np.ones((N, 2))
-    Z[:,0] = X
-    Z[:,1] = np.sin(X)
-    return Z
-
-def __find_min_poly_basis(X_nearest):
-    y_test = [9504, 9530, 9541, 9557, 9585, 9585, 9585, 9627, 9654, 9664, 9699]
+# try many for diff values of k and p
+def __find_min_poly_basis(X, y, num_data, log_base=False, use_prediction=True):
+    # first is for phase 1, second is for phase 2
+    # y_test = [9504,9530,9541,9557,9585,9585,9585,9627,9654,9664,9699]
+    y_test = [9504,9530,9541,9557,9585,9585,9585,9627,9654,9664,9699,9722,9746,9760,9778,9794,9829,9862,9888,9922]
 
     min_error = np.inf
 
-    ca_cases = X_ca["cases"].values
-    ca_100k = X_ca["cases_100k"].values
-    ca_14_100k = X_ca["cases_14_100k"].values
-
-    for p1 in range(1,4):
-        for p2 in range(1,4):
-            for p3 in range(1,6):
-                for k in range(30, 211):
-                    ca_cases_trans = __polyBasis(ca_cases, p1)
-                    ca_100k_trans = __polyBasis(ca_100k, p2)
-                    ca_14_100k_trans = __polyBasis(ca_14_100k, p3)
-
-                    X = np.column_stack((ca_cases_trans, ca_100k_trans, ca_14_100k_trans))
-                    X = np.append(X, X_nearest, axis=1)
-
-                    # fit to autogressor model
-                    model = AutoRegressor(k=k)
-                    model.fit(X, y_ca)
-                    y_preds = model.predict(X)
-
-                    error = np.sum(np.square(y_preds-y_test))
-                    if (error < min_error):
-                        p1_low = p1
-                        p2_low = p2
-                        p3_low = p3
-                        k_low = k
-                        min_error = error
-    
-    print("lowest p1: %d, p2: %d, p3: %d, k:%d, error: %.3f" % (p1_low, p2_low, p3_low, k_low, min_error)) 
-
-def __find_min_sin_basis(X_nearest):
-    y_test = [9504, 9530, 9541, 9557, 9585, 9585, 9585, 9627, 9654, 9664, 9699]
-
-    min_error = np.inf
-
-    ca_cases = X_ca["cases"].values
-    ca_100k = X_ca["cases_100k"].values
-    ca_14_100k = X_ca["cases_14_100k"].values
-
-    for p in range(1,6):
-        for k in range(30, 211):
-            ca_cases_trans = __sinBasis(ca_cases)
-            ca_100k_trans = __sinBasis(ca_100k)
-            ca_14_100k_trans = __polyBasis(ca_14_100k, p)
-
-            X = np.column_stack((ca_cases_trans, ca_100k_trans, ca_14_100k_trans))
-            X = np.append(X, X_nearest, axis=1)
-
-            # fit to autogressor model
-            model = AutoRegressor(k=k)
-            model.fit(X, y_ca)
-            y_preds = model.predict(X)
-
+    for p in range(1,5):
+        for k in range(1, 280):
+            model = AutoRegressor(k=k, p=p, log_base=log_base, use_prediction=use_prediction)
+            try:
+                model.fit(X, y)
+            except:
+                continue
+            y_preds = model.predict(X, num_data)
+            if log_base == True:
+                y_preds = np.exp(y_preds)
             error = np.sum(np.square(y_preds-y_test))
+            # print("p: %d, k: %d, error: %.3f" % (p, k, error))
             if (error < min_error):
                 p_low = p
                 k_low = k
                 min_error = error
     
-    print("lowest p: %d, k:%d, error: %.3f" % (p_low, k_low, min_error)) 
+    print("lowest p: %d, k: %d, error: %.3f" % (p_low, k_low, min_error)) 
 
+def __plot(x, y, fname, label, xlabel, ylabel):
+    plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%m/%d/%Y'))
+    plt.gca().xaxis.set_major_locator(mdates.MonthLocator())
+    plt.plot(x, y)
+    plt.gcf().autofmt_xdate()
+    plt.title(label)
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+    plt.savefig(os.path.join("..", "figs", fname))
+    plt.clf()
 
 # main
 if __name__ == "__main__":
-    phase = "phase1"
-    num_data = 11
+    # modify variables here
+    phase = "phase2"
+    num_data = 5
+    log_base = True
+    use_prediction = True
 
+    # load file + set data
     df = pd.read_csv(os.path.join("..", "data", phase + "_training_data.csv"))
     
     X_codes = df.country_id.unique()
@@ -114,52 +75,45 @@ if __name__ == "__main__":
     ca_cases = X_ca["cases"].values
     ca_100k = X_ca["cases_100k"].values
     ca_14_100k = X_ca["cases_14_100k"].values
-    y_ca = X_ca["deaths"].values
-
-    ca_initial = np.column_stack((ca_cases, ca_100k, ca_14_100k))
-
-    # get similar data sets
-    # knn = KNN(k=1)
-    # knn.fit(ca_initial, y_ca)
-    # X_similar = np.ones(X_codes.size)
-    # self_pred = knn.predict(ca_initial)
-    # self_val = np.max(np.bincount(self_pred))
-    # i = 0
-    # for code in X_codes:
-    #     data = df.loc[df["country_id"] == code]
-    #     X_test = np.array(data[["cases", "cases_100k", "cases_14_100k"]].values)
-    #     pred = knn.predict(X_test)
-    #     if pred.size == 0: continue
-    #     X_similar[i] = np.max(np.bincount(pred))
-    #     i = i + 1
-
-    # # use nearest = abs(X_similar - self_val) < 2
-    # nearest = X_codes[np.abs(X_similar - self_val) < 2]
-
-    # generate feature with basis
-    ca_cases_trans = __polyBasis(ca_cases, 3)
-    ca_100k_trans = __polyBasis(ca_100k, 1)
-    ca_14_100k_trans = __polyBasis(ca_14_100k, 1)
+    ca_deaths = X_ca["deaths"].values
+    dates = X_ca["date"].values
 
     # combine column
-    X = np.column_stack((ca_cases_trans, ca_100k_trans, ca_14_100k_trans))
+    X = np.column_stack((ca_cases, ca_100k, ca_14_100k, ca_deaths))
 
-    # append nearest to training data
-    # X_nearest = np.zeros(shape=(X.shape[0], nearest.size*3))
-    # i = 0
-    # for code in nearest:
-    #     data = df.loc[df["country_id"] == code]
-    #     vals = data[["cases", "cases_100k", "cases_14_100k"]].values
-    #     X_nearest[:, i:i+3] = vals
-    #     i+=3
+    # if we are using log space
+    if log_base == True:
+        X = np.log(X)
+        X[X == -np.inf] = 0
 
-    # X = np.append(X, X_nearest.mean(axis=1).reshape(-1,1), axis=1)
-    # __find_min_sin_basis(X_nearest)
+        ca_deaths = np.log(ca_deaths)
+        ca_deaths[ca_deaths == -np.inf] = 0
+
+    # run test only in phase 1 mode
+    # __find_min_poly_basis(X, ca_deaths, num_data, log_base, use_prediction)
+    
+    # code for plots
+    # dates_obj = [dt.datetime.strptime(d, "%m/%d/%Y").date() for d in dates]
+    # labels = ["cases vs date", "cases_100k vs date", "cases_14_100k vs date", "deaths vs date"]
+    # ylabels= ["cases", "cases_100k", "cases_14_100k", "deaths"]
+    # fnames = ["cases", "cases_100k", "cases_14_100k", "deaths"]
+
+    # for i in range (4):
+    #     if log_base == True:
+    #         __plot(dates_obj, X[:,i], fnames[i]+"_log", labels[i] + " (log)", "date", ylabels[i] + " (log)")
+    #     else:
+    #         __plot(dates_obj, X[:,i], fnames[i], labels[i], "date", ylabels[i])
+        
 
     # fit to autogressor model
-    model = AutoRegressor(k=42)
-    model.fit(X, y_ca)
+    model = AutoRegressor(k=79, p=1, log_base=log_base, use_prediction=use_prediction)
+    model.fit(X, ca_deaths)
     y_preds = model.predict(X, num_data)
+
+    if log_base == True:
+        y_preds = np.exp(y_preds)
+
+    y_preds = y_preds.astype('int64')
     print(y_preds)
     
     # to csv
--- src/model/auto_regressor.py
+++ src/model/auto_regressor.py
@@ -2,75 +2,174 @@ import numpy as np
 from model.linear_model import LeastSquares
 
 class AutoRegressor:
-    def __init__(self, k):
+    def __init__(self, k, p=1, 
+                log_base=False,
+                use_prediction=True,
+                model=LeastSquares):
+        """
+        k                   = the number of data sets to look behind to make next prediction
+        p                   = polynomial basis
+        log_base            = change data to log space
+        use_prediction       = use prediction from X_k to test y_k+1, if false uses
+                              time series the the deaths column itself
+        model               = the linear model to use. We only use Least Squares
+        In this class, everything with variable X means it is not weighted
+        everything with variable Z means it has been transformed/weighted.
+        """
         self.k = k
-        self.x_model = LeastSquares()
-        self.y_model = LeastSquares()
+        self.p = p
+        self.log_base = log_base
+        self.use_prediction = use_prediction
+        self.model = model
+        self.y_model = model()
 
     def fit(self, X, y):
         N,D = X.shape
-        k=self.k
+        k = self.k
 
+        # if k is too large don't do this.
+        self.size = N - k
+        if (self.size <= 0): return
+
+        # transform X to Z using transform method.
         Z = self.__transform(X)
-        X_t = X[k+1:N, :]
-        y_t = y[k+1:N]
+        # get values of y for time series
+        y_k = y[self.size:N]
 
-        self.X_t = X_t
-        self.y_t = y_t
-        # now we have model for X
-        self.x_model.fit(Z, X_t)
-        # models for y
-        self.y_model.fit(Z, y_t)
+        # set our features to predict next features.
+        self.__feature_time_series(X, Z)
+        # our general prediction for y_k+1
+        self.y_model.fit(Z, y_k)
+
+    def __feature_time_series(self, X, Z):
+        """
+        Grabs data from k to N-1 for each feature. If p > 0, also adds polynomial basis
+        columns.
+        Generates models to predict x_i_k+1 so that we can comput y_k+1.
+        The models here are by independent, meaning they will have their own
+        bias when fitting and predicting the model.
+        """
+        N, D = X.shape
+        p = self.p
+
+        feature_models = []
+        for i in range(D):
+            pos = i*p+1
+            # grab the data from Z and add a bias 1 or e.
+            x_i = self.__add_bias(Z[:,pos:pos+p])
+            x_k = X[self.size:N,i]
+            # create model and fit.
+            model = self.model()
+            model.fit(x_i, x_k)
+            # save model to predict x_i_k+1
+            feature_models.append(model)
+
+        self.feature_models = feature_models
 
     def __transform(self, X):
         """
-        Transform data X to Z for time series
-                       1 ... T-k                  1 ... k
-        Columns of Z = 1 ... T-k+1   = rows of X  1 ... k  
-                       ...                        ...
+        Transforms X into Z. Adds the bias 1 if non logbase, e if logbase.
+        Adds polynomial basis given p.
         """
         N,D = X.shape
         k = self.k
+        p = self.p
 
-        size = N-k-1
-        Z = np.ones(shape=(size, D*k+1))
+        # new data set
+        Z = np.ones(shape=(k, D*p+1))
+        k_data = X[self.size-1:N-1, :]
 
-        for i in range(k):
-            start = i*D+1
-            end = start+D
-            Z[:, start:end] = X[i+1:size+i+1, :]
+        for i in range(D):
+            pos = i*p+1
+            # add polynomial basis
+            Z[:,pos:pos+p] = self.__polyBasis(k_data[:,i])
+
+        # handle log case
+        if self.log_base == True:
+            Z[:,0] = np.exp(1)
 
         return Z
 
+    def __polyBasis(self, X):
+        """
+        General function to change to poly basis.
+        NOTE: this does not add 1 as a bias.
+        """
+        N = X.size
+        p = self.p
 
-    def predict(self, X, num_times=11):
-        def append_time_series(Z, x_pred):
+        Z = np.ones((N, p))
+        for j in range(1, p + 1):
+            Z[:,j-1] = X**j
+
+        return Z
+
+    def __add_bias(self, X):
+        """
+        Adds a bias of e if log_base, 1 otherwise.
+        """
+        N,D = X.shape
+        Z = np.ones(shape=(N,D+1))
+        Z[:,1:] = X
+
+        if self.log_base == True:
+            Z[:,0] = np.exp(1)
+
+        return Z
+
+    def predict(self, X, num_times):
+        """
+        Predict the next sequence of num_times.
+        """
+        def append_time_series(Z, y_k):
             """
-            Get new Z with calculated x_pred for time series
+            Predict x_i_k+1, removes x_i_1 from Z, and appends the new value.
+            If use_prediction is on, it will use the predicted value y_k+1 and its 
+            polynomial biases for next predict. Else it will use the values from
+            its features series x_i_k+1.
             """
             N,D = Z.shape
-            n = x_pred.size
+            p = self.p
+            feature_models = self.feature_models
 
+            i = 0
+            time_preds = np.ones(shape=(1,D))
+            for model in feature_models:
+                # predict the new features
+                pos = i*p+1
+                x_i = self.__add_bias(Z[:,pos:pos+p])
+                pred = model.predict(x_i)[-1]
+                i += 1
+
+                time_preds[:,pos:pos+p] = self.__polyBasis(pred)
+
+            # determine log base
+            if self.log_base == True:
+                time_preds[:,0] = np.exp(1)
+
+            # remove first row, append to last row.
             Z = Z[1:]
-            new_t = np.zeros(shape=(D))
+            Z = np.append(Z, time_preds, axis=0)
 
-            new_t[:-n] = Z[-1,n:]
-            new_t[-n:] = x_pred
-            Z = np.append(Z, new_t.reshape(1,-1), axis=0)
+            # use prediction will change the deaths column to predicted value and its
+            # polynomial basis.
+            if self.use_prediction == True:
+                Z[:,pos:pos+p] = self.__polyBasis(y_k)
 
             return Z
 
         Z = self.__transform(X)
-        x_pred = self.x_model.predict(Z)[-1] # first pred to get next y
+        y_k = self.y_model.predict(Z)[-1]
         
-        y_preds = np.zeros(num_times)
+        y_pred = np.zeros(num_times)
 
         for i in range(num_times):
-            Z = append_time_series(Z, x_pred)
-            y_preds[i] = self.y_model.predict(Z)[-1]
-            x_pred = self.x_model.predict(Z)[-1]
+            # predict, and save results.
+            Z = append_time_series(Z, y_k)
+            y_k = self.y_model.predict(Z)[-1]
+            y_pred[i] = y_k
 
-        return y_preds
+        return y_pred
 
 
 
